c21c0fdfc57e75d74de2a3cc0dae6f067aa10a83
ttttt07 <om.nom.omg.t@gmail.com>
2023-05-28 23:02:38 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function sum(a,b){
+    console.log(a+b);
+}
+
+function substract (a,b){
+    console.log(a-b);
+}
+
+function multiply (a,b){
+    console.log(a*b);
+}
+
+function divide (a,b){
+ if(a==0){
+    alert("error")
+ }else{
+    console.log(a/b);
+ }
+}
+
+divide(0,4);
